dcfa597bcfc5338e9e4663c07aad235b87e978a9
saarp <saarp@liveperson.com>
2020-12-24 19:21:33 +0200

    rm

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-// add this file to .gitignore
-
-module.exports = {
-    google: {
-        clientID: '248307541515-3r2011rvqt86jhsms7g9pqvgnl8223ge.apps.googleusercontent.com',
-        clientSecret: '3P3ocu6g8PsRl6Lrt1J_WvZE'
-    }
-};
