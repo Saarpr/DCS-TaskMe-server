9d842efa14c5f97352b7217f01210f588dcd2357
saarp <saarp@liveperson.com>
2020-12-29 18:36:25 +0200

    make it better

--- Routers/auth-routh.js
+++ Routers/auth-routh.js
@@ -1,6 +1,10 @@
 const authRouter = require('express').Router();
 const passport = require('passport');
 
+// auth login
+authRouter.get('/login', (req, res) => {
+    res.render('login', { user: req.user });
+});
 
 // auth logout
 authRouter.get('/logout', (req, res) => {
