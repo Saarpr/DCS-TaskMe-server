a9f003ad8802f964f3fa7790d96948fc2947cbbd
saarp <saarp@liveperson.com>
2020-12-29 23:02:43 +0200

    make it better

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(passport.session());
 app.use(express.static("./DCS-TaskMe-client/"));
 /////////////////Login/////////////////////
 app.get('/', (req, res)=> {
-  res.sendFile(__dirname + "/DCS-TaskMe-client/home.html");
+  res.sendFile( "/DCS-TaskMe-client/home.html");
 });
 
 app.use('/auth',authRouter);
