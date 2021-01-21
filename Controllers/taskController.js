23ab8bc1a5bcb8f8fc8d9134234eb79ed76f5b64
saarp <saarp@liveperson.com>
2021-01-21 09:57:08 +0200

    fix update task api

--- Controllers/taskController.js
+++ Controllers/taskController.js
@@ -1,6 +1,7 @@
 const Task = require('../Models/task');
 
 exports.tasksController = {
+
     getTasks(req, res) {
         const queryPar = req.query;
         if(Object.keys(queryPar).length == 0){              // if there is'nt query
@@ -14,29 +15,34 @@ exports.tasksController = {
             .catch(err => console.log(`Error getting restaurant from db: ${err}`));
         }
      },
+
     getTask(req, res) {
         Task.findOne({ _id : req.params.taskId })
             .then(result => { res.json(result)})
      },
+
     addTask(req, res) {
         const { body } = req;
         const newTask = new Task(body);
         const result = newTask.save();
         if(result){
-            res.json(newRestaurant);
+            res.json(newTask);
         } else {
          res.status(404).send("Error saving a restaurant");
         }
      },
-     searchTask(req,res){
+
+    searchTask(req,res){
         const query = req.query;
         // const findIt = "/" + query.searchBy + "/";
-        if(query.searchBy){
-            let query = Task.search( "blue" );
-            Task.find(query)
+         console.log(req.query.searchBy)
+        if(req.query.searchBy){
+            let result = Task.search( req.query.searchBy );
+            Task.find(result)
                 .then(result => { res.json(result)});
         }
     },
+
     deleteTask(req, res) {
         Task.deleteOne({ _id: req.params.taskId } ) 
         .then(docs => { res.json(docs) })
@@ -44,24 +50,10 @@ exports.tasksController = {
     },
 
     updateTask(req, res) {
-        console.log('updateee');
-        // const { body } = req;
-        // Task.findOne({googleId:req.params.id})
-        //     .then(node => {
-        //         let item = node.tasks.find(o => o._id == body._id)
-        //         console.log("this is body 2" , body)
-        //         if(body.taskName)
-        //             item["taskName"] = body.taskName
-        //         if(body.color)
-        //             item["color"] = body.color
-        //         if(body.duration)
-        //             item["duration"] = body.duration
-        //         if(body.dateTime)
-        //             item["taskName"] = body.dateTime
-        //         console.log("this is item" , item)
-        //         console.log("this is node" , node)
-        //         return node.save()
-        //             .then(node => res.send(node.tasks))})
-
+        const {body} = req;
+        Task.findOneAndUpdate({_id:body._id}, body, {new: true,useFindAndModify: false}, (err, task) => {
+            if (err) return res.status(500).send(err);
+            return res.send(task);
+        })
     }
-    };
+};
