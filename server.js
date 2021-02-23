20bb734b796d4701d1d89f21fa02b7e7515fa614
saarp <saarp@liveperson.com>
2021-02-23 12:27:40 +0200

    add trust proxy

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('trust proxy',1);
 app.use(session({
     key: "userId",
     secret: process.env.COOKIEKEY,
