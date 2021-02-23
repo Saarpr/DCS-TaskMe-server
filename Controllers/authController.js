04b2e39f71f87ed4ceeb0777614939308d49dfcb
saarp <saarp@liveperson.com>
2021-02-23 13:27:51 +0200

    debug verification link

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,24 +1,24 @@
 const User = require('../Models/user');
-const { OAuth2Client, UserRefreshClient } = require('google-auth-library');
+const {OAuth2Client, UserRefreshClient} = require('google-auth-library');
 const client = new OAuth2Client(process.env.G_ID);
 // mailgun
 const mailgun = require("mailgun-js");
 const DOMAIN = process.env.MAILGUN_DOMAIN;
-const mg = mailgun({ apiKey: process.env.MAILGUN_APIKEY, domain: DOMAIN });
+const mg = mailgun({apiKey: process.env.MAILGUN_APIKEY, domain: DOMAIN});
 // json web token
 const jwt = require('jsonwebtoken');
-const { json } = require('express');
+const {json} = require('express');
 const bcrypt = require('bcrypt');
 
 
 exports.authController = {
     signUp(req, res) {
-        const { name, email, password } = req.body;
-        User.findOne({ email }).exec((err, user) => {
+        const {name, email, password} = req.body;
+        User.findOne({email}).exec((err, user) => {
             if (user) {
-                return res.status(400).json({ error: "User with this email already exists." });
+                return res.status(400).json({error: "User with this email already exists."});
             }
-            const token = jwt.sign({ name, email, password }, process.env.JWT_TOKEN_ACTIVATE, { expiresIn: '30m' });
+            const token = jwt.sign({name, email, password}, process.env.JWT_TOKEN_ACTIVATE, {expiresIn: '30m'});
             const data = {
                 from: 'noreply@TaskMe.com',
                 to: email,
@@ -42,72 +42,71 @@ exports.authController = {
         })
     },
     activateAccount(req, res) {
-        const { token } = req.body; //maybe get by headers
-        console.log("activating:" , token )
+        const {token} = req.body; //maybe get by headers
+        console.log("activating:", token)
         if (token) {
             jwt.verify(token, process.env.JWT_TOKEN_ACTIVATE, function (err, decodedToken) {
                 if (!decodedToken) {
-                    res.status(400).json({ error: "Incorrect or Expired link!" })
+                    res.status(400).json({error: "Incorrect or Expired link!"})
                 }
-                const { name, email, password } = decodedToken;
-                console.log("name:" , name )
-                console.log("email:" , email )
-                console.log("password:" , password )
+                const {name, email, password} = decodedToken;
+                console.log("name:", name)
+                console.log("email:", email)
+                console.log("password:", password)
                 let tmpPassword;
                 bcrypt.genSalt(10, function (err, salt) {
                     console.log("Encrypting ")
-                    bcrypt.hash(password , salt, function (error, hashPass) {
-                        if (err){
+                    bcrypt.hash(password, salt, function (error, hashPass) {
+                        if (err) {
                             return console.log(error);
                         }
                         console.log(hashPass)
                         tmpPassword = hashPass;
                     });
-                });
-                console.log("tmpPassword:" , tmpPassword )
-
-                User.findOne({ email }).exec((err, user) => {
+                }).then(
+                User.findOne({email}).exec((err, user) => {
                     if (user) {
-                        return res.status(400).json({ error: "User with this email already exists." })
+                        return res.status(400).json({error: "User with this email already exists."})
                     }
-                    let newUser = new User({ name, email, password: tmpPassword });
+                    let newUser = new User({name, email, password: tmpPassword});
                     newUser.save((err, success) => {
                         if (err) {
                             console.log("Error in signup while account activation: ", err);
-                            return res.status(400), json({ error: 'error activating account' });
+                            return res.status(400), json({error: 'error activating account'});
                         }
                         res.json({
                             message: "Signup success!"
                         })
                     })
                 })
+            )
             })
         } else {
-            return res.json({ error: "something went wrong!" })
+            return res.json({error: "something went wrong!"})
         }
     },
 
     signIn(req, res) {
-        const { email, password } = req.body;
-        User.findOne({ email:email }).exec((err, user) => {
-            if (!user) {      
+        const {email, password} = req.body;
+        User.findOne({email: email}).exec((err, user) => {
+            if (!user) {
                 return res.status(401).json({
                     error: "This user does not exist, signup first!"
                 })
             }
-            bcrypt.compare(password, user.password , function(err, result) {
-                if(err)
+            bcrypt.compare(password, user.password, function (err, result) {
+                if (err)
                     return res.status(401).json({
                         error: "Email or password incorrect!"
                     })
-                if(result){
-                    const token = jwt.sign({ _id: user._id }, process.env.JWT_SIGNIN_KEY, { expiresIn: '3h' });
-                    const { name, email, picture } = user;
-                    req.session.user = { name, email, picture };
-                    res.json({token, user: { name, email, picture }
+                if (result) {
+                    const token = jwt.sign({_id: user._id}, process.env.JWT_SIGNIN_KEY, {expiresIn: '3h'});
+                    const {name, email, picture} = user;
+                    req.session.user = {name, email, picture};
+                    res.json({
+                        token, user: {name, email, picture}
                     })
-                }
-                else {
+                } else {
                     return res.status(401).json({
                         error: "Email or password incorrect!"
                     })
@@ -117,58 +116,58 @@ exports.authController = {
 
         })
     },
-    checkAuth(req,res){
-        if(req.session.user){
-            res.send({loggedIn: true , user: req.session.user})
-        }else{
+    checkAuth(req, res) {
+        if (req.session.user) {
+            res.send({loggedIn: true, user: req.session.user})
+        } else {
             res.send({loggedIn: false})
         }
     },
-    logout(req,res){
-        if(req.session.user){
+    logout(req, res) {
+        if (req.session.user) {
             req.session.destroy();
             res.send({loggedIn: false});
-        }else{
+        } else {
             res.send({loggedIn: false});
         }
     },
 
     googleLogin(req, res) {
-        const { tokenId } = req.body;
-        client.verifyIdToken({ idToken: tokenId, audience: process.env.G_ID }).then(response => {
-            const { email_verified, name, email , picture } = response.payload;
+        const {tokenId} = req.body;
+        client.verifyIdToken({idToken: tokenId, audience: process.env.G_ID}).then(response => {
+            const {email_verified, name, email, picture} = response.payload;
             // console.log(response.payload);
             if (email_verified) {
-                User.findOne({ email }).exec((err, user) => {
+                User.findOne({email}).exec((err, user) => {
                     if (err) {
                         return res.status(400).json({
                             error: "something went wrong!"
                         })
                     } else {
                         if (user) {
-                            const token = jwt.sign({ _id: user._id }, process.env.JWT_SIGNIN_KEY, { expiresIn: '3h' });
+                            const token = jwt.sign({_id: user._id}, process.env.JWT_SIGNIN_KEY, {expiresIn: '3h'});
                             //creat session here
-                            const { _id, name, email, picture } = user;
-                            req.session.user = { name, email, picture };
+                            const {_id, name, email, picture} = user;
+                            req.session.user = {name, email, picture};
                             res.json({
                                 token,
-                                user: { _id, name, email, picture }
+                                user: {_id, name, email, picture}
                             })
                         } else {
-                            let password = email+process.env.JWT_SIGNIN_KEY;
-                            let newUser = User({name , email , password , picture});
+                            let password = email + process.env.JWT_SIGNIN_KEY;
+                            let newUser = User({name, email, password, picture});
                             newUser.save((err, data) => {
-                                if(err) {
+                                if (err) {
                                     return res.status(400).json({
                                         error: "something went wrong"
                                     })
                                 }
-                                const token = jwt.sign({ _id: newUser._id }, process.env.JWT_SIGNIN_KEY, { expiresIn: '3h' });
-                                req.session.user = { name, email, picture };
-                                const { name, email, picture } = newUser;
+                                const token = jwt.sign({_id: newUser._id}, process.env.JWT_SIGNIN_KEY, {expiresIn: '3h'});
+                                req.session.user = {name, email, picture};
+                                const {name, email, picture} = newUser;
                                 res.json({
                                     token,
-                                    user: { name, email, picture }
+                                    user: {name, email, picture}
                                 })
                             })
                         }
